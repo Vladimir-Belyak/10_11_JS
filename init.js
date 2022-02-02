22d26e04857932eb86885688316fdf71c7b70c4d
Vladimir-Belyak <wabelyak@mail.ru>
2022-02-02 23:41:39 +0700

    1st upload, 1 of 4

---
+++ init.js
@@ -0,0 +1,8 @@
+window.onload = function()
+{
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('surnameOutput').innerText = initPerson.SurName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.BirthYear;
+};
